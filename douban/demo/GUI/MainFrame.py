7c424cd737b80c1a7712d221da5fd6c6d67fafa6
OrangeGuo <1772086465@qq.com>
2019-07-05 23:55:37 +0800

    test ui

--- douban/demo/GUI/MainFrame.py
+++ douban/demo/GUI/MainFrame.py
@@ -1,16 +1,5 @@
 from tkinter import *         
-root = Tk()                    
-
-li     = ['C','python','php','html','SQL','java']
-movie  = ['CSS','jQuery','Bootstrap']
-listb  = Listbox(root)         
-listb2 = Listbox(root)
-for item in li:   
-    listb.insert(0,item)
- 
-for item in movie:              
-    listb2.insert(0,item)
- 
-listb.pack()                    
-listb2.pack()
-root.mainloop()                 
+win=Tk()
+win.title('豆瓣电影Top250')
+win.geometry("400x400")
+win.mainloop()       
